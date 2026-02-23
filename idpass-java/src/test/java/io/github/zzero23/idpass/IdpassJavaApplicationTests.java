1bafa9e54a3dec9d296b85a3bea743a0ba173094
zzero23 <devji0ng23@gmail.com>
2026-02-23 10:30:40 +0900

    ✨ feat: Java/Python/React 통합 프로젝트 구조 구축

---
+++ idpass-java/src/test/java/io/github/zzero23/idpass/IdpassJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zzero23.idpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpassJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
